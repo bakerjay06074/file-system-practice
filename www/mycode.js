cc2205a7712d0275f2d58f14ab6e7750fa868214
jbaker <detroiterinct@gmail.com>
2017-01-17 12:47:54 -0500

    added alert
    Added alert to show entry into createFile
    Added semicolon to end of alert inside success callback for creating fileEntry

--- www/mycode.js
+++ www/mycode.js
@@ -16,11 +16,12 @@ function createFile() {
    var type = window.TEMPORARY;
    var size = 5*1024*1024;
 
+   alert('starting to execute createFile');
    window.requestFileSystem(type, size, successCallback, errorCallback)
 
    function successCallback(fs) {
       fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
-         alert('File creation successfull!')
+         alert('File creation successfull!');
       }, errorCallback);
    }
 
